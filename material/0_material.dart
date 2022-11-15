72ddd39d84cbbd18383bf08b508809128439f644
juan <jjhuertasbotache@gmail.com>
2022-11-15 05:56:59 -0500

    day 2 learning intermediate dart

---
+++ material/0_material.dart
@@ -0,0 +1,16 @@
+
+List people = ["Juan",
+ "Jose",
+ "Alvaro",
+ "Eduardo",
+ "Laura",
+ "Victoria"
+ ];
+
+void sayHello(name) {
+  print("hello ${name}!!");
+}
+
+void main(List<String> args) {
+  print(people);
+}
